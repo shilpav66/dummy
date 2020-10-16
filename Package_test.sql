78dbb85fa4117e5bfb2030d487a25cf5caf5b2b3
shilpav66 <shilpav66@gmail.com>
2020-10-16 11:42:10 +0200

    Update Package_test.sql

--- Package_test.sql
+++ Package_test.sql
@@ -1,14 +1,39 @@
-CREATE PACKAGE Test_jenkins AS
-   
-END Test_jenkins;
+--
+-- TEST_Scipt  (Package) 
+--
+CREATE OR REPLACE PACKAGE Test_Scipt
+AS
+/******************************************************************************
+   NAME:       test
+   PURPOSE:
+
+   REVISIONS:
+   Ver        Date        Author           Description
+   ---------  ----------  ---------------  ------------------------------------
+   1.0        28-Dec-2006             1. Created this package.
+******************************************************************************/
+   FUNCTION tfunction (param1 IN NUMBER)
+      RETURN NUMBER;
+
+END Test_Scipt;
 /
-CREATE PACKAGE BODY Test_jenkins AS
--- the following parameter declaration raises an exception 
--- because 'DATE' does not match employees.hire_date%TYPE
--- PROCEDURE calc_bonus (date_hired DATE) IS
--- the following is correct because there is an exact match
-   BEGIN
-     DBMS_OUTPUT.PUT_LINE('Employees hired on ' || date_hired || ' get bonus.');
-   END;
-END Test_jenkins;
+--
+-- Test_Scipt  (Package Body) 
+--
+CREATE OR REPLACE PACKAGE BODY Test_Scipt AS
+/******************************************************************************
+   NAME:       test
+   PURPOSE:
+
+   REVISIONS:
+   Ver        Date        Author           Description
+   ---------  ----------  ---------------  ------------------------------------
+   1.0        28-Dec-2006             1. Created this package body.
+******************************************************************************/
+
+  FUNCTION tFunction(Param1 IN NUMBER) RETURN NUMBER IS
+  BEGIN
+    RETURN Param1;
+  END;
 /
+
