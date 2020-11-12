a8479cebee43e64ce098a5af1074549074144d3c
shilpav66 <shilpav66@gmail.com>
2020-11-12 13:07:45 +0100

    Update Package_test.sql

--- Package_test.sql
+++ Package_test.sql
@@ -1,7 +1,7 @@
 --
 -- TEST_Scipt  (Packaging) 
 --
-CREATE OR REPLACE PACKAGE 
+CREATE OR REPLACE PACKAGE TEST_Scipt
 AS
 /******************************************************************************
    NAME:       test
